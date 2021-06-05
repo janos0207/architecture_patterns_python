4a08e0f7d5410ad7f89ab513a8de1042c6d8900e
y_fukuda <colors92window@gmail.com>
2021-06-05 12:27:27 +0900

    can only deallocate allocated lines

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from datetime import date
-from typing import Optional
+from typing import Optional, Set
 
 
 @dataclass(frozen=True)
@@ -15,11 +15,25 @@ class Batch:
     def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
         self.reference = ref
         self.sku = sku
-        self.available_quantity = qty
         self.eta = eta
+        self._purchased_quantity = qty
+        self._allocations: Set[OrderLine] = set()
 
     def allocate(self, line: OrderLine):
-        self.available_quantity -= line.qty
+        if self.can_allocate(line):
+            self._allocations.add(line)
 
-    def can_allocate(self, line: OrderLine):
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self):
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self):
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
