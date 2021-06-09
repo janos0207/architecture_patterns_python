ff4829bafc463ea41156d6ba20d81b0cdd2714d8
y_fukuda <colors92window@gmail.com>
2021-06-10 00:41:57 +0900

    an allocation prefers earlier batches

--- test_allocate.py
+++ test_allocate.py
@@ -1,8 +1,9 @@
 from datetime import date, timedelta
 from model import allocate, Batch, OrderLine
 
-now = date.today()
-tomorrow = now + timedelta(days=1)
+today = date.today()
+tomorrow = today + timedelta(days=1)
+later = today + timedelta(weeks=1)
 
 def test_prefers_current_stock_batches_to_shipments():
     in_stock_batch = Batch('in-stock-batch', 'RETRO-CLOCK', 100, eta=None)
@@ -13,3 +14,15 @@ def test_prefers_current_stock_batches_to_shipments():
 
     assert in_stock_batch.available_quantity == 90
     assert shipment_batch.available_quantity == 100
+
+def test_prefers_earlier_batches():
+    earliest = Batch('speedy-batch', 'MINIMALIST-SPOON', 100, eta=today)
+    medium = Batch('normal-batch', 'MINIMALIST-SPOON', 100, eta=tomorrow)
+    latest = Batch('slow-batch', 'MINIMALIST-SPOON', 100, eta=later)
+    line = OrderLine('order1', 'MINIMALIST-SPOON', 10)
+
+    allocate(line, [earliest, medium, latest])
+
+    assert earliest.available_quantity == 90
+    assert medium.available_quantity == 100
+    assert latest.available_quantity == 100
