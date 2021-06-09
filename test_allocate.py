34dcea0ef36eaada84ac664dca680ea2aeba843c
y_fukuda <colors92window@gmail.com>
2021-06-10 08:27:59 +0900

    allocate returns allocated batch ref

--- test_allocate.py
+++ test_allocate.py
@@ -5,6 +5,7 @@ today = date.today()
 tomorrow = today + timedelta(days=1)
 later = today + timedelta(weeks=1)
 
+
 def test_prefers_current_stock_batches_to_shipments():
     in_stock_batch = Batch('in-stock-batch', 'RETRO-CLOCK', 100, eta=None)
     shipment_batch = Batch('shipment-batch', 'RETRO-CLOCk', 100, eta=tomorrow)
@@ -14,6 +15,7 @@ def test_prefers_current_stock_batches_to_shipments():
 
     assert in_stock_batch.available_quantity == 90
     assert shipment_batch.available_quantity == 100
+
 
 def test_prefers_earlier_batches():
     earliest = Batch('speedy-batch', 'MINIMALIST-SPOON', 100, eta=today)
@@ -26,3 +28,13 @@ def test_prefers_earlier_batches():
     assert earliest.available_quantity == 90
     assert medium.available_quantity == 100
     assert latest.available_quantity == 100
+
+
+def test_returns_allocated_batch_ref():
+    in_stock_batch = Batch('in-stock-batch-ref', 'HIGHBROW-POSTER', 100, eta=None)
+    shipment_batch = Batch('shipment-batch-ref', 'HIGHBROW-POSTER', 100, eta=tomorrow)
+    line = OrderLine('oref', 'HIGHBROW-POSTER', 10)
+
+    allocation = allocate(line, [in_stock_batch, shipment_batch])
+
+    assert allocation == in_stock_batch.reference
