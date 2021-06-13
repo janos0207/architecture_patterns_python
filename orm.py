1e3780b3b856db4ad397ba147e3b1d105b61a948
y_fukuda <colors92window@gmail.com>
2021-06-13 17:57:47 +0900

    table definition for OrderLine

---
+++ orm.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import mapper, relationship
+from sqlalchemy.schema import MetaData, Table
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import Integer, String
+import model
+
+metadata = MetaData()
+
+order_lines = Table(
+    'order_lines', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('sku', String(255)),
+    Column('qty', Integer, nullable=False),
+    Column('orderod', String(255)),
+)
+
+
+def start_mappers():
+    line_mapper = mapper(model.OrderLine, order_lines)
