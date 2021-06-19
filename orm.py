a70f66dc39e706fa60853c66d1b51d4e43b276b5
y_fukuda <colors92window@gmail.com>
2021-06-19 09:46:21 +0900

    test saving allocations

--- orm.py
+++ orm.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import mapper, relationship
 from sqlalchemy.schema import MetaData, Table
-from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.schema import Column, ForeignKey
 from sqlalchemy.sql.sqltypes import Date, Integer, String
 import model
 
@@ -23,7 +23,21 @@ batches = Table(
     Column('eta', Date, nullable=True),
 )
 
+allocations = Table(
+    'allocations', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('orderline_id', ForeignKey('order_lines.id')),
+    Column('batch_id', ForeignKey('batches.id')),
+)
+
 
 def start_mappers():
-    line_mapper = mapper(model.OrderLine, order_lines)
-    mapper(model.Batch, batches)
+    lines_mapper = mapper(model.OrderLine, order_lines)
+    mapper(
+        model.Batch,
+        batches,
+        properties={
+            "_allocations": relationship(
+                lines_mapper, secondary=allocations, collection_class=set,
+            )
+        })
--- test_orm.py
+++ test_orm.py
@@ -54,3 +54,14 @@ def test_saving_batch(session):
         'SELECT reference, sku, _purchased_quantity, eta FROM "batches"'
     )
     assert list(rows) == [("batch1", "sku1", 100, None)]
+
+
+def test_saving_allocations(session):
+    batch = model.Batch("batch1", "sku1", 100, eta=None)
+    line = model.OrderLine("order1", "sku1", 10)
+    batch.allocate(line)
+    session.add(batch)
+    session.commit()
+    rows = list(session.execute(
+        'SELECT orderline_id, batch_id FROM "allocations"'))
+    assert rows == [(batch.id, line.id)]
